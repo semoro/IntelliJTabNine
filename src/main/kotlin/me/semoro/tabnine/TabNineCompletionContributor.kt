edb4790cd70c795caf986020f71d3b63913b8d01
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-07-18 22:40:58 +0300

    Update copyright

--- src/main/kotlin/me/semoro/tabnine/TabNineCompletionContributor.kt
+++ src/main/kotlin/me/semoro/tabnine/TabNineCompletionContributor.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2019-2019 Simon Ogorodnik.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package me.semoro.tabnine
 
 import com.intellij.codeInsight.completion.*
